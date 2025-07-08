b8d2653773d921cd2e6d74a4afd02e69bf0b0754
diouf332 <iandiouf8@gmail.com>
2025-07-08 10:46:54 +0300

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://iboxbot1:1111@cluster0.ndunpsw.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Diouf")
+DATABASE_NAME = environ.get('DATABASE_NAME', "diouf")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others
